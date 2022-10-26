25d2a71290a93fba50432601c3e4caac0584caa0
dakshayanim <111320659+dakshayanim@users.noreply.github.com>
2022-10-27 04:36:06 +0530

    Add files via upload

---
+++ rainbow.py
@@ -0,0 +1,13 @@
+#Hello!!This is a challenge on python package called termcolor
+#My gratitude to our instructor Ryan Mitchell.
+#To start this program we need to install termcolor package-->pip install termcolor
+
+CODE:
+from termcolor import colored
+
+print(colored('///RAINBOW///','red'))
+print(colored('///RAINBOW///','yellow'))
+print(colored('///RAINBOW///','green'))
+print(colored('///RAINBOW///','cyan'))
+print(colored('///RAINBOW///','blue'))
+print(colored('///RAINBOW///','magennta'))
